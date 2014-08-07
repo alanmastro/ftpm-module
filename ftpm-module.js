495ce288ed4efeed188ac296da21d3dedd7014d2
Marco Bicchi <marco@join-italia.com>
2014-08-07 16:22:50 +0200

    Bugfix for multiple callback inside async system like 'async' for nodejs

--- ftpm-module.js
+++ ftpm-module.js
@@ -2,33 +2,38 @@ var ftpm = require('./lib/ftpm');
 
 var ftpmModule = {
 	installOsFont: function(fontName, cb) {
-		ftpm.callbackFunction = cb;
+		ftpm.callbackLink = cb;
 		ftpm.emit('runDriver', 'osfont', 'install', fontName);
 	},
 	uninstallOsFont: function(fontName, cb) {
 		fontName = fontName.toTitleCase();
-		ftpm.callbackFunction = cb;
+        ftpm.callbackAlreadyFired = false;
+		ftpm.callbackLink = cb;
 		if (existsSync(ftpm.path.getFontPath(ftpm.platform) + fontName.removeSpaces() + '.ftpm.ttf')) {
 			ftpm.emit('runDriver', 'osfont', 'uninstall', fontName);
 		}
 	},
 	listIntalledFonts: function(cb) {
-		ftpm.callbackFunction = cb;
+        ftpm.callbackAlreadyFired = false;
+		ftpm.callbackLink = cb;
 		ftpm.emit('runDriver', 'osfont', 'local', '');
 	},
 	downloadWebFont: function(fontName, fontPath, cb) {
-		ftpm.callbackFunction = cb;
+        ftpm.callbackAlreadyFired = false;
+		ftpm.callbackLink = cb;
 		ftpm.outputPath = fontPath;
 		ftpm.emit('runDriver', 'webfont', 'web', fontName);
 	},
 	getCssFont: function(fontName, fontPath, cb) {
-		ftpm.callbackFunction = cb;
+        ftpm.callbackAlreadyFired = false;
+		ftpm.callbackLink = cb;
 		ftpm.outputPath = fontPath;
 		ftpm.showContent = (!fontPath) ? true : false;
 		ftpm.emit('runDriver', 'cssfont', 'css', fontName);
 	},
 	getDataURI: function(fontName, fontPath, cb) {
-		ftpm.callbackFunction = cb;
+        ftpm.callbackAlreadyFired = false;
+		ftpm.callbackLink = cb;
 		ftpm.outputPath = fontPath;
 		ftpm.showContent = (!fontPath) ? true : false;
 		ftpm.emit('runDriver', 'cssfont', 'datauri', fontName);
